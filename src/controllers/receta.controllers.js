35b77a47ed476e8292ae2fc68912dff865f12ea1
SergioRuiz <sergioruizrock@gmail.com>
2024-03-26 05:14:40 -0300

    creamos el controlador para las recetas y creamos la funcion crear receta

---
+++ src/controllers/receta.controllers.js
@@ -0,0 +1,16 @@
+import Tarea from "../database/models/tarea.js"
+
+export const crearReceta = async(req,res)=>{
+    try {
+        const nuevaReceta = new Tarea(req.body);
+
+        await nuevaReceta.save()
+
+        res.status(201).json("Se guardo una nueva Receta!")
+    } catch (error) {
+        console.error(error)
+        res.status(400).json({
+            mensaje: "error: no se pudo crear la Receta"
+        })
+    }
+}
